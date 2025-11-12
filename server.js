c48cee982dffffa44fdd7b6137697c6a59676732
SNSAIKRISHNA <saikrishnasnoff@gmail.com>
2025-11-12 12:41:43 +0530

    commit-3

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import http from 'node:http'
 import { serveStatic } from './utils/serveStatic.js'
+import { handleGet } from './handlers/routeHandlers.js'
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
-    await serveStatic(req, res, __dirname)
-}) 
 
-server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
+    if (req.url === '/api') {
+        if (req.method === 'GET') {
+            return await handleGet(res)
+        }
+    }
+    else if (!req.url.startsWith('/api')) {
+        return await serveStatic(req, res, __dirname)
+    }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
